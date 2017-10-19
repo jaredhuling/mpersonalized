962bf1f657d6610615516fa8c98fb3e0cea41764
chenshengkuang <chenshengkuang@gmail.com>
2017-10-19 17:06:30 -0500

    More detailed description of the predict function.

--- R/predict.R
+++ R/predict.R
@@ -1,13 +1,13 @@
-#' @title Predict Function for "mp" object
+#' @title Predict for "mp" object
 #'
 #' @description This function predict the benefit scores and optimal treatment for new patients
 #'
-#' @param mp the fitted "mp" object
-#' @param newx the covariate matrix of the new patients. If newx = NULL, then the prediction is made for each data set based on the rule of that study.
+#' @param mp the fitted "mp" object returned by "mpersonalized"
+#' @param newx the covariate matrix of the new patients. If newx = NULL, then the prediction is made for each data set based on the rule of that study/outcome.
 #' @param overall_rec a logical value. If \code{TRUE}, an overall recommendation will be made weighted by the "weight" parameter.
 #' @param weight a weight vector for the overall recommendation. If leave as \code{NULL}, a equally weighted recommendation will be made.
 #'
-#' @return
+#' @return a list of results with each element in the list corresponding to the prediciton by using each penalty parameter. Each element in this list contains:
 #' \item{treatment}{recommended treatment for each patient for each study/outcome. If \code{overall_rec = TRUE}, the weighted overall recommended treatment will be computed as well.
 #' If the overall recommened treatment is equal to 0.5, it means the sum of weight is equal for 0 and 1.}
 #' \item{benefit_score}{the benefit score computed from \eqn{g_1, \dots, g_K}}
@@ -109,16 +109,16 @@ predict.mp = function(mp, newx = NULL, weight = NULL, overall_rec = TRUE){
 }
 
 
-#' @title Predict Function for "mp_cv" object
+#' @title Predict for "mp_cv" object
 #'
 #' @description This function predict the benefit scores and optimal treatment for new patients
 #'
-#' @param mp_cv the fitted "mp_cv" object
-#' @param newx the covariate matrix of the new patients. If newx = NULL, then the prediction is made for each data set based on the rule of that study.
+#' @param mp_cv the fitted "mp_cv" object returned from "mpersonalized_cv" function
+#' @param newx the covariate matrix of the new patients. If newx = NULL, then the prediction is made for each data set based on the rule of that study/outcome.
 #' @param overall_rec a logical value. If \code{TRUE}, an overall recommendation will be made weighted by the "weight" parameter.
 #' @param weight a weight vector for the overall recommendation. If leave as \code{NULL}, a equally weighted recommendation will be made.
 #'
-#' @return
+#' @return the prediciton by using the optimal penalty parameter selected by cross validation. It contains:
 #' \item{treatment}{recommended treatment for each patient for each study/outcome. If \code{overall_rec = TRUE}, the weighted overall recommended treatment will be computed as well.
 #' If the overall recommened treatment is equal to 0.5, it means the sum of weight is equal for 0 and 1.}
 #' \item{benefit_score}{the benefit score computed from \eqn{g_1, \dots, g_K}}
