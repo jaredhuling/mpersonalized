b484d0441535a9eb9e3a04979ab1c1d700a3885b
chenshengkuang <chenshengkuang@gmail.com>
2018-03-29 23:11:45 -0500

    fixed bug

--- R/lambda_estimate.R
+++ R/lambda_estimate.R
@@ -7,50 +7,50 @@ lambda_estimate =  function(modelXlist, modelYlist, penalty, single_rule, alpha,
 
   #need to estimate lambda1?
   if (single_rule == FALSE &
-      penalty != "fused" &
-      is.null(lambda1)){
+      penalty != "fused"){
+    if (is.null(lambda1)){
+      q = length(modelXlist)
+      p = dim(modelXlist[[1]])[2]
 
-    q = length(modelXlist)
-    p = dim(modelXlist[[1]])[2]
+      x = as.matrix(bdiag(modelXlist))
+      y = unlist(modelYlist)
+      total_n = length(y)
 
-    x = as.matrix(bdiag(modelXlist))
-    y = unlist(modelYlist)
-    total_n = length(y)
+      x = sqrt(total_n) * x; y = sqrt(total_n) * y
 
-    x = sqrt(total_n) * x; y = sqrt(total_n) * y
+      data = list(x = x, y = y)
+      SGL_lambda = SGL(data = data, index = rep(1 : p, q),
+                       alpha = alpha, standardize = FALSE, nlam = num_lambda1)$lambdas
+      lambda1 = SGL_lambda
 
-    data = list(x = x, y = y)
-    SGL_lambda = SGL(data = data, index = rep(1 : p, q),
-                     alpha = alpha, standardize = FALSE, nlam = num_lambda1)$lambdas
-    lambda1 = SGL_lambda
-
-    lambda_estimate$lambda1 = lambda1
+      lambda_estimate$lambda1 = lambda1
+    }
   }
 
   #need to estimate lambda2?
   if (single_rule == FALSE &
-      penalty %in% c("fused", "lasso+fused", "GL+fused", "SGL+fused") &
-      is.null(lambda2)){
+      penalty %in% c("fused", "lasso+fused", "GL+fused", "SGL+fused")){
+    if  (is.null(lambda2)){
+      q = length(modelXlist)
+      p = dim(modelXlist[[1]])[2]
 
-    q = length(modelXlist)
-    p = dim(modelXlist[[1]])[2]
+      x = as.matrix(bdiag(modelXlist))
+      y = unlist(modelYlist)
+      D = NULL
+      for (i in 2:q)
+        for (j in 1:(i-1)){
+          newrow = numeric(q)
+          newrow[i] = -1
+          newrow[j] = 1
+          D = rbind(D, newrow)
+        }
+      D = as.matrix(bdiag(replicate(p, D, simplify = FALSE)))
 
-    x = as.matrix(bdiag(modelXlist))
-    y = unlist(modelYlist)
-    D = NULL
-    for (i in 2:q)
-      for (j in 1:(i-1)){
-        newrow = numeric(q)
-        newrow[i] = -1
-        newrow[j] = 1
-        D = rbind(D, newrow)
-      }
-    D = as.matrix(bdiag(replicate(p, D, simplify = FALSE)))
+      genlasso_lambda = genlasso(y = y, X = x, D = D)$lambda
+      lambda2 = quantile(genlasso_lambda, probs = seq(1, 0, -1 / (num_lambda2 - 1)), names = FALSE)
 
-    genlasso_lambda = genlasso(y = y, X = x, D = D)$lambda
-    lambda2 = quantile(genlasso_lambda, probs = seq(1, 0, -1 / (num_lambda2 - 1)), names = FALSE)
-
-    lambda_estimate$lambda2 = lambda2
+      lambda_estimate$lambda2 = lambda2
+    }
   }
 
 
